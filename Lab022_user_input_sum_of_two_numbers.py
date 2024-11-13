111015d483863d4c049c9056b8a6ad58ff139b6a
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-14 02:01:02 +0530

    09 nov

---
+++ Lab022_user_input_sum_of_two_numbers.py
@@ -0,0 +1,38 @@
+# write a program to take the 2 user input
+# then sum the number
+# mul -> *
+# div -> /
+
+#logic Building
+# step 1
+#I/P -> num1, num2 -> int
+#O/P -> sum -> int, sub -> int, div -> float
+
+num1 = int(input("enter the num1"))
+num2 = int(input("enter the num2"))
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+print(type(num1))
+print(type(num2))
+
+#step 2 / Rough logic
+#sum +, -, *, /
+
+#str -> int
+int()
+
+#step 3
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("sum is :", sum)
+print("sub is :", sub)
+print("mul is :", mul)
+print("div is :", div)
+
+#num1 -> 155
+#num2 -> 5
