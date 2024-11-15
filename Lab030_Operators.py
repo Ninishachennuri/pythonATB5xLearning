d52a454af3d3ea454aeae4422e53960d0c3884db
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 19:19:01 +0530

    9th nov task2

---
+++ Lab030_Operators.py
@@ -0,0 +1,19 @@
+#Operators
+
+#age = 65
+# age reference variable name | identifier
+# = -> Operator
+# 65 -> literal | variable name
+
+# unary - operator - which is added to operand
+
+age = +65
+age2 = -65
+
+print(age)
+print(age2)
+
+# - - unary operator
+how_many_lambo_ninisha = -1
+how_many_lambo_ninisha = how_many_lambo_ninisha + 1
+print(how_many_lambo_ninisha)
