f9a4d3d4fdc6560ec81fa4c0e291589da1698e32
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 22:34:02 +0530

    12 class operator

---
+++ lab039_Operator_p8.py
@@ -0,0 +1,21 @@
+q, r = divmod(5,2)
+print(q)
+print(r)
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+#Increment (++) / decrement  (--) operator
+# good news - ++, --
+x = 5
+x += 1 # x = x + 1
+print(x)
+
+x -= 2 # x = x - 2
+print(x)
+
+x *= 3 # x = x - 3
+print(x)
+
---
+++ lab041_user_input_ternary_operator.py
@@ -0,0 +1,12 @@
+# program - if age > 18 - allowed to vote
+# else - not allowed too
+
+
+user_age = int(input("enter your age\n"))
+
+if user_age > 18
+    print("yes you can go to goa and vote")
+else:
+    print("not you can't go and can't vote")
+
+print("yes you can go to goa and vote" if user_age > 18 else "not you can't go and can't vote")
