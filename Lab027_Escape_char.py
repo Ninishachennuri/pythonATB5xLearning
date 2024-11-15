6b258f97c4a7714f0195ade98f53ab62e030f95b
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 11:38:38 +0530

    09 nov

---
+++ Lab027_Escape_char.py
@@ -0,0 +1,21 @@
+#Escape _Sequences
+#\n -> new line
+#\t -> tab
+#\b -> back space (1 char backspace)
+
+print("Hello\nworld")
+print("Hello\tworld")
+print("Hello\bworld")
+
+#' ' or " "
+c = 'c'
+c1 = "c"
+
+# where this r concept will be use
+dir = r'c:\Ninisha\n.txt'
+dir = r"c:\Ninisha\t.txt"
+print(dir)
+
+
+print("Ninisha Chennuri")
+print(r"Ninisha C\thennuri")
