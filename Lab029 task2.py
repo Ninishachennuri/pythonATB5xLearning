421e5b459ce26b8cf750c436681ca1d72b04e808
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 13:19:02 +0530

    9th nov task2

---
+++ Lab029 task2.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the num1"))
+num2 = int(input("Enter the num2"))
+
+div = num1 / num2
+print(div)
+
+q = num1 / num2
+print(q)
+print(type(q))
+
+r = num1 % num2
+print(r)
+print(type(r))
