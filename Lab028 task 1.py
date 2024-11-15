311556b6fb515f19c3547c953e30dfa75d46c64c
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 12:20:34 +0530

    9th nov task1

---
+++ Lab028 task 1.py
@@ -0,0 +1,21 @@
+#Task for today
+#Take a 3 nput form the user
+#perform sum, sub, mul, div
+
+num1 = int(input("Enter the num1"))
+num2 = int(input("Enter the num2"))
+num3 = int(input("Enter the num3"))
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+sum = num1 + num2 + num3
+sub = num1 - num2 - num3
+mul = num1 * num2 * num3
+div = num1 / num2 / num3
+
+print("sum is :", sum)
+print("sub is :", sub)
+print("mul is :", mul)
+print("div is :", div)
