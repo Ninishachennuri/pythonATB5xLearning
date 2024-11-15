a37f7a19392e8c4d247379ecac3d8d7fb17aaf2b
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-15 22:59:38 +0530

    12 class operator

---
+++ lab042_task1.py
@@ -0,0 +1,11 @@
+# program - if age > 25 - allowed to go to club
+# else - not allowed too
+
+user_age = int(input("enter your age"))
+
+if user_age > 25:
+    print("you can go to the club")
+else:
+    print("you can't go to the club")
+
+#print("yes you can go to club"if user_age > 25 else "not you cant go to club")
