38ab47f1b168be19b937e190c0b5689487284765
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-21 00:07:20 +0530

    16 class loop

--- ex_16112024_Loops/lab061_for_if_Task.py
+++ ex_16112024_Loops/lab061_for_if_Task.py
@@ -12,8 +12,3 @@ for i in range(1, 101):
     else:
         print(i)
 
-# | i | condition | o / p
-# | 0 | 0%2 == 0 -> True | o/p -> 0
-# | 1 | 1%2 == 0 -> False | o/p -> Nothing will be printed
-# | 2 | 2%2 == 0 -> True | o/p -> 2
-# | 3 | 3%2 == 0 -> False | o/p -> Nothin willbe printed
