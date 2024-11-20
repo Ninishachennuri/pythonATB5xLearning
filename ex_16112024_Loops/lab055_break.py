e8f4749ab2cf2e05d6b7630a60192003b3ffe60c
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-20 23:03:26 +0530

    16 class loop

---
+++ ex_16112024_Loops/lab055_break.py
@@ -0,0 +1,13 @@
+for i in range(0, 10): # 0 to 9, times ->
+    print(i)
+    if i == 5:
+        break
+
+
+# |i | condition | o/p
+# |0 | 0 == 5 -> False | o/p = 0
+# |1 | 1 == 5 -> False | o/p = 1
+# |2 | 2 == 5 -> False | o/p = 2
+# |3 | 3 == 5 -> False | o/p = 3
+# |4 | 4 == 5 -> False | o/p = 4
+# |5 | 5 == 5 -> True | o/p = BREAK out of for loop
