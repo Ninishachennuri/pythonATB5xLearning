0cad26f712e7721533d4ee74fe07712f2b8471a8
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-20 21:19:21 +0530

    16 class loop

---
+++ ex_16112024_Loops/lab053_while.py
@@ -0,0 +1,24 @@
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+# i = 0, 0 < 10, o/p -> 0
+# i = 1, 1 < 10, o/p -> 1
+# i = 9, 9 < 10, o/p -> 9
+# i = 10, 10 < 10, o/p -> Exit the loop
+print("------")
+
+i = 0
+#while i < 5:
+while i <= 5:
+    print(i)
+    i = i + 1
+
+
+# i = 0 , 0 < 5 -> 0
+# i = 1 , 1 < 5 -> 1
+# i = 2 , 2 < 5 -> 2
+# i = 3 , 3 < 5 -> 3
+# i = 4 , 4 < 5 -> 4
+# i = 5 , 5 < 5 -> out of the loop
