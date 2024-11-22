fe524849502a8242e5a05b3040d30d08b3cdbc17
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-22 18:02:51 +0530

    19 class functions

---
+++ ex_19112024_Functions/lab062_Functions.py
@@ -0,0 +1,14 @@
+#Basic Functions
+
+#part 1 - Define
+
+def greet():
+    print("Hello, Welcome to python world!")
+
+# part 2 - call
+greet()
+greet()
+greet()
+
+print("jhdjckfh")
+greet()
---
+++ ex_19112024_Functions/lab063_Functions_part2.py
@@ -0,0 +1,16 @@
+#Basic Functions
+
+#part 1 - Define
+
+def greet():
+    print("Hello, Welcome to python world!")
+    print("2")
+    print("3")
+
+# part 2 - call
+greet()
+greet()
+greet()
+
+print("jhdjckfh")
+greet()
