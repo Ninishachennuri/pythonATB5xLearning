b299ccfb44345e9d32bb3705f9b7d0d934d39778
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-11-27 19:23:41 +0530

    21 class tasks

---
+++ ex_21112024_Functions_Scopes/lab088_Task 1.py
@@ -0,0 +1,8 @@
+#Right Triangle Start Pattern
+
+print("*")
+for i in range(1,5):
+    for j in range(1,5):
+        if  j <= i:
+            print("*",end="")
+    print("*")
---
+++ ex_21112024_Functions_Scopes/lab089_Task 2.py
@@ -0,0 +1,9 @@
+#Left Triangle Start Pattern
+
+
+for i in range(1,5):
+    for j in range(1,5):
+        if  i <= j:
+            print("*",end="")
+    print("*")
+print("*")
