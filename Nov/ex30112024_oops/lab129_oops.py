1323bea12459c2dc2cf3b8fa1dca4483720c7609
Ninisha Chennuri <ninishachennuri30@gmail.com>
2024-12-06 23:15:31 +0530

    30 class

---
+++ Nov/ex30112024_oops/lab129_oops.py
@@ -0,0 +1,15 @@
+a = 10
+
+
+class Person:
+    b = 11 # Instance - belong to class
+
+    def print_info(self):
+        c = 20 #Local variable
+        print(c)
+        print(self.b)
+        global a
+        print(a)
+
+object_ref = Person()
+object_ref.print_info()
